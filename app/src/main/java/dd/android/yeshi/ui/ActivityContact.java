909ec228608a7fea51fcdd216c5a024ee40fd118
destinyd <destinyd_war@163.com>
2013-04-07 13:08:11 +0800

    tab image and contact activity

---
+++ app/src/main/java/dd/android/yeshi/ui/ActivityContact.java
@@ -0,0 +1,19 @@
+package dd.android.yeshi.ui;
+
+import android.os.Bundle;
+import com.actionbarsherlock.view.MenuItem;
+import dd.android.yeshi.R;
+
+
+public class ActivityContact extends ActivityYS {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.act_contact);
+
+//        getSupportActionBar().setHomeButtonEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
